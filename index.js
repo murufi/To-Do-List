31a785f1d1b3bd7f680a24a766328beaab171263
Abubakar Rufai [Soft_Code] <68183191+murufi@users.noreply.github.com>
2023-08-10 13:56:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const taskInput = document.getElementById('taskInput')
-// const taskTime = document.getElementById('taskTime')
+const taskTime = document.getElementById('taskTime')
 const taskList = document.getElementById('taskList')
 const addButton = document.getElementById('addButton')
 
 const addTask = ()=>{
     const addText = taskInput.value;
-
-    let timepic;
-
-    timepic = document.createElement('input')
-    timepic.setAttribute('type', 'time')
-    timepic.setAttribute('class', 'addTime')
     const addTime = taskTime.value;
 
     if (addText.trim() !== '') {
@@ -25,7 +19,6 @@ const addTask = ()=>{
 
         taskItem.innerHTML = `${addText} ${dateTime} | ${addTime}`;
         taskList.appendChild(taskItem);
-        timepic.append(taskItem)
         taskInput.value = '';
 
     }
